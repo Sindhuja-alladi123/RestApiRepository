ab1179de5798fab6bd5da3c81428ad48080113e3
Sindhuja Uppala <sindhuja.uppala@gmail.com>
2021-03-10 21:53:55 +0530

    added courseapi

---
+++ course-api/src/main/java/io/javabrains/springboot/Hello/HelloController.java
@@ -0,0 +1,19 @@
+package io.javabrains.springboot.Hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/Hello")
+	public String sayHi()
+	{
+		return "Hi";
+	}
+	@RequestMapping("/welcome")
+	public String Welcome()
+	{
+		return "welcomepage";
+	}
+}
